767577c5a0318e12a496bd9469fef64db3a9deaa
John Kaul <john7kaul@gmail.com>
2018-12-13 10:00:24 -0600

    (mf)cmakelists.txt, maiin.cpp: Initial commit.

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+// File Last Updated: 10.19.18 13:00:54
+//
+//: argument-parser
+//
+// BY  :
+#include <iostream>
+#include <cstdlib>
+
+//----------------------------------------------------------------------
+// argument parser      This is an argument parser playground.
+//
+// ARGS
+//    VARIABLE        :    VARIABLE-DESCRIPTION
+//----------------------------------------------------------------------
+
+int main(int argc, const char *argv[]) {
+//     TSymtab globalArgumentSymtab;
+
+    /*
+    This obviously wont work.
+
+    TSymtabNode *pNode = globalArgumentSymtab.Search( argv[1] );
+    if ( !pNode ) {
+        pNode = globalArgumentSymtab.Enter( argv[1] );
+    }
+*/    
+    return 0;
+} ///:~
+
